9dedb54dd65d25214e0c974240a608dac1592008
Arshpreet Makker <amakker01@gmail.com>
2020-05-30 00:08:13 +0530

    This is created in lieu of completion of Peer Review Assignment of Week 1.

---
+++ Plot2.R
@@ -0,0 +1,6 @@
+# Construct plot  2 
+plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+# Save PNG file
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
